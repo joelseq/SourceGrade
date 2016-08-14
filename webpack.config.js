012ca686dfe7d582a76e1b8e2b934a0736849e79
Joel Sequeira <joelseq96@gmail.com>
2016-08-13 23:45:26 -0700

    Add webpack and app entry point

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var webpack = require('webpack');
+
+module.exports = {
+  entry: [
+    'script!jquery/dist/jquery.min.js',
+    'script!foundation-sites/dist/foundation.min.js',
+    './app/app.jsx'
+  ],
+  externals: {
+    jquery: 'jQuery'
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      '$': 'jquery',
+      'jQuery': 'jquery'
+    })
+  ],
+  output: {
+    path: __dirname,
+    filename: './public/bundle.js'
+  },
+  resolve: {
+    root: __dirname,
+    alias: {
+    },
+    extensions: ['', '.js', '.jsx']
+  },
+  module: {
+    loaders: [{
+      loader: 'babel-loader',
+      query: {
+        presets: ['react', 'es2015', 'stage-0']
+      },
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/
+    }]
+  },
+  devtool: 'cheap-module-eval-source-map'
+};
