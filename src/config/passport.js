6ec1d3f95ba51fa3a0b01c38a30d7bf27ea4ae98
Joel Sequeira <joelseq96@gmail.com>
2016-09-15 17:54:22 -0700

    Small change to passport config file

--- src/config/passport.js
+++ src/config/passport.js
@@ -1,4 +1,3 @@
-const passport = require('passport');
 const User = require('../models/user');
 const JwtStrategy = require('passport-jwt').Strategy;
 const ExtractJwt = require('passport-jwt').ExtractJwt;
