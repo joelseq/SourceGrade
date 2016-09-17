351ce50856f61b328273729fd42f6d01772e16b3
Joel Sequeira <joelseq96@gmail.com>
2016-09-16 23:35:00 -0700

    Add Select Component to make classes searchable

--- app/containers/GradesForm.jsx
+++ app/containers/GradesForm.jsx
@@ -1,22 +1,33 @@
 import React, { Component, PropTypes } from 'react';
+import Select from 'react-select';
+import * as actions from '../actions';
+import { connect } from 'react-redux';
 
-export default class GradesForm extends Component {
+//Styles for react-select
+import 'react-select/dist/react-select.css';
+
+class GradesForm extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       id: '',
+      class: '',
       url: ''
     };
 
     this.onFormSubmit = this.onFormSubmit.bind(this);
     this.onIdChange = this.onIdChange.bind(this);
-    this.onUrlChange = this.onUrlChange.bind(this);
+    this.handleClassChange = this.handleClassChange.bind(this);
   }
 
   static contextTypes = {
     router: PropTypes.object
   };
+
+  componentWillMount() {
+    this.props.getClasses();
+  }
 
   onFormSubmit(e) {
     e.preventDefault();
@@ -34,13 +45,27 @@ export default class GradesForm extends Component {
     });
   }
 
-  onUrlChange(e) {
+  handleClassChange(val) {
     this.setState({
-      url: e.target.value
+      class: val,
+      url: val.value
     });
   }
 
   render() {
+    let options = [];
+
+    if(this.props.classes) {
+      options = this.props.classes.map(current => {
+        return {
+          value: current.url,
+          label: current.courseName
+        };
+      });
+    }
+
+
+
     return (
       <div id="grades-form" className="row">
         <div className="columns medium-6 small-centered">
@@ -48,11 +73,31 @@ export default class GradesForm extends Component {
           <h3 className="text-center">Search for your grades</h3>
           <form onSubmit={this.onFormSubmit}>
             <input value={this.state.id} type="text" onChange={this.onIdChange} placeholder="Secret Number"/>
-            <input value={this.state.url} type="text" onChange={this.onUrlChange} placeholder="Course URL"/>
+            <Select
+              name="class-form"
+              value={this.state.class}
+              placeholder="Search for a Class"
+              options={options}
+              onChange={this.handleClassChange}
+              className="select"
+              clearable={false}
+            />
             <button className="button expanded">Get Grades</button>
           </form>
         </div>
       </div>
     );
+
+    return (
+      <input value={this.state.url} type="text" onChange={this.onUrlChange} placeholder="Course URL"/>
+    );
   }
 }
+
+function mapStateToProps(state) {
+  return {
+    classes: state.classes.classes,
+  };
+}
+
+export default connect(mapStateToProps, actions)(GradesForm);
