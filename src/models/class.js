d775709f5a5fecc0540fdea50ce4a752c4c51edb
Joel Sequeira <joelseq96@gmail.com>
2016-09-18 18:53:45 -0700

    Add changes to make sure that no duplicate classes can be added

--- src/models/class.js
+++ src/models/class.js
@@ -2,8 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const classSchema = new Schema({
-  courseName: { type: String, required: true, unique: true },
-  url: { type: String, required: true }
+  courseName: { type: String, required: true },
+  url: { type: String, required: true, unique: true }
 });
 
 module.exports = mongoose.model('class', classSchema);
