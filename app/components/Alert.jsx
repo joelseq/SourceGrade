3957462fc34836ebd522645f4f08b92588d30774
Joel Sequeira <joelseq96@gmail.com>
2016-09-18 23:13:16 -0700

    Refactor Alert to make it a stateless component

--- app/components/Alert.jsx
+++ app/components/Alert.jsx
@@ -1,16 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 
-export default class Alert extends Component {
-  render() {
-    const { message, className, onClose } = this.props;
+export default function(props) {
+  const { message, className, onClose } = props;
 
-    return (
-      <div id="alert" className={`callout ${className}`} data-closable>
-        {message}
-        <button onClick={onClose} className="close-button" aria-label="Dismiss alert" type="button" data-close>
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-    );
-  }
+  return (
+    <div id="alert" className={`callout ${className}`} data-closable>
+      {message}
+      <button onClick={onClose} className="close-button" aria-label="Dismiss alert" type="button" data-close>
+        <span aria-hidden="true">&times;</span>
+      </button>
+    </div>
+  );
 }
