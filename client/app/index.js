3963466eabc119ba5f1929905731d626d1f142a1
Joel Sequeira <joelseq96@gmail.com>
2018-01-22 02:53:42 -0800

    Add basic client side setup

--- client/app/index.js
+++ client/app/index.js
@@ -1,30 +1,20 @@
 import React from 'react';
 import { render } from 'react-dom';
+import { Provider } from 'react-redux';
+import { ConnectedRouter } from 'react-router-redux';
+import store, { history } from './store';
 
-import {
-  BrowserRouter as Router,
-  Route,
-  Link,
-  Switch
-} from 'react-router-dom'
-
-import App from './components/App/App';
-import NotFound from './components/App/NotFound';
-
-import Home from './components/Home/Home';
-
-import HelloWorld from './components/HelloWorld/HelloWorld';
+import App from './components/App';
 
 import './styles/styles.scss';
 
-render((
-  <Router>
-    <App>
-      <Switch>
-        <Route exact path="/" component={Home}/>
-        <Route path="/helloworld" component={HelloWorld}/>
-        <Route component={NotFound}/>
-      </Switch>
-    </App>
-  </Router>
-), document.getElementById('app'));
+render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <div>
+        <App />
+      </div>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById('app')
+);
