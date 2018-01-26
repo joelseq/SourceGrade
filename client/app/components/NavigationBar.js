bc6d53b9e3592424b50040af9612475c27cdf03f
Joel Sequeira <joelseq96@gmail.com>
2018-01-26 03:01:21 -0800

    Refactor NavBar to show links based on auth state

--- client/app/components/NavigationBar.js
+++ client/app/components/NavigationBar.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
 import { NavLink } from 'react-router-dom';
 import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, NavItem } from 'react-bootstrap';
 
-const NavigationBar = () => (
+const NavigationBar = ({ authenticated }) => (
   <Navbar>
     <Navbar.Header>
       <Navbar.Brand>
@@ -11,14 +13,40 @@ const NavigationBar = () => (
       </Navbar.Brand>
     </Navbar.Header>
     <Nav pullRight>
-      <LinkContainer to="login">
-        <NavItem eventKey={1}>Login</NavItem>
-      </LinkContainer>
-      <LinkContainer to="signup">
-        <NavItem eventKey={2}>Sign Up</NavItem>
-      </LinkContainer>
+      {authenticated
+        ?
+          [
+            <LinkContainer key="classes" to="classes">
+              <NavItem eventKey={1}>Classes</NavItem>
+            </LinkContainer>,
+            <LinkContainer key="logout" to="logout">
+              <NavItem eventKey={2}>Logout</NavItem>
+            </LinkContainer>,
+          ]
+        :
+          [
+            <LinkContainer key="login" to="login">
+              <NavItem eventKey={3}>Login</NavItem>
+            </LinkContainer>,
+            <LinkContainer key="signup" to="signup">
+              <NavItem eventKey={4}>Sign Up</NavItem>
+            </LinkContainer>,
+          ]
+      }
     </Nav>
   </Navbar>
 );
 
-export default NavigationBar;
+NavigationBar.propTypes = {
+  authenticated: PropTypes.bool,
+};
+
+NavigationBar.defaultProps = {
+  authenticated: false,
+};
+
+const mapStateToProps = state => ({
+  authenticated: state.auth.authenticated,
+});
+
+export default connect(mapStateToProps)(NavigationBar);
